bfcc9c0acebe1bd8a6fb9644e16272bf81c23e59
erolbe1 <erolbe1@corp.onespan.com>
2020-03-23 10:12:16 -0400

    sound added.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../App.css';
+
 import { Timer } from './Timer';
 import { TimeSet } from './TimeSet';
 import { Control } from './Controls';
 
 
 import { useInterval } from '../hooks/UseInterval';
+
+import alarm from '../sounds/alarm.mp3'
 
 
 const App = () => {
@@ -14,6 +17,7 @@ const App = () => {
   const [mode, setMode] = useState('session');
   const [time, setTime] = useState(sessionVal * 60 * 1000);
   const [active, setActive] = useState(false);
+  const beep = useRef();
 
   useInterval(() => setTime(time - 1000), active ? 1000 : null)
 
@@ -23,15 +27,19 @@ const App = () => {
 
   useEffect(() => {
     if (time === 0 && mode === 'session') {
+      beep.current.play();
       setMode('break')
       setTime(breakVal * 60 * 1000)
     } else if (time === 0 && mode === 'break') {
+      beep.current.play();
       setMode('session')
       setTime(sessionVal * 60 * 1000)
     }
   }, [time, breakVal, sessionVal, mode])
 
   const handleReset = () => {
+    beep.current.pause()
+    beep.current.currentTime = 0
     setActive(false)
     setMode('session')
     setBreakVal(5)
@@ -53,8 +61,8 @@ const App = () => {
           <TimeSet type={'BREAK'} value={[breakVal, setBreakVal]} />
           <TimeSet type={'SESSION'} value={[sessionVal, setSessionVal]} />
         </div>
-      </div> 
-    
+      </div>
+      <audio id="beep" src={alarm} ref={beep} />
     </div>
   );
 }
